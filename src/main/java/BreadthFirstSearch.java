5fe6b07739042ee0b3d60f183ec908006ea1afbe
SantiagoFirpoEdu <96509711+SantiagoFirpoEdu@users.noreply.github.com>
2023-05-24 19:56:07 -0300

    Implemented breadth-first search

--- src/main/java/BreadthFirstSearch.java
+++ src/main/java/BreadthFirstSearch.java
@@ -1,8 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BreadthFirstSearch
 {
     public BreadthFirstSearch(WeightedGraph weightedGraph, int startingVertice)
     {
-        //TODO: implement this
+        int edgeAmount = weightedGraph.getEdgeAmount();
+        visited = new boolean[edgeAmount];
+        predecessor = new int[edgeAmount];
+        distance = new int[edgeAmount];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startingVertice);
+
+        while (!queue.isEmpty())
+        {
+            Integer toBeVisited = queue.poll();
+            visited[toBeVisited] = true;
+
+            var edges = weightedGraph.getEdges(toBeVisited);
+            for (WeightedGraph.Edge edge : edges)
+            {
+                int neighbor = edge.to;
+                if (visited[neighbor])
+                {
+                    continue;
+                }
+
+                visited[neighbor] = true;
+                predecessor[neighbor] = toBeVisited;
+                distance[neighbor] += edge.weight;
+                queue.add(neighbor);
+            }
+        }
     }
 
     public void debugPrintResult()
